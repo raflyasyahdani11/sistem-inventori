374950bb15edd4de11bc5dc6a714f5b96a3d3a04
fahtur_rf <fahturrahman18ti@mahasiswa.pcr.ac.id>
2023-03-24 16:28:50 +0700

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,39 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+import { globSync } from "glob";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const jsCustom = Object.fromEntries(
+    globSync("resources/js/pages/**/*.[js/css]").map((file) => [
+        path.relative(
+            "resources/js/pages",
+            file.slice(0, file.length - path.extname(file).length)
+        ),
+        fileURLToPath(new URL(file, import.meta.url)),
+    ])
+);
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js", jsCustom],
             refresh: true,
         }),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    let extension =
+                        assetInfo.name?.split(".").at(1) ?? "compiled";
+
+                    return `${extension}/[name].[hash][extname]`;
+                },
+                chunkFileNames: "js/chunks/[name].[hash].js", // all chunks output path
+                entryFileNames: "js/[name].[hash].js", // all entrypoints output path
+            },
+        },
+    },
 });
